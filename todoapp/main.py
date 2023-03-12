121612bfe3268c863a09d64570508c87a1426268
Boram Jeong <annajeong@microsoft.com>
2023-03-12 20:09:34 +0900

    Initial commit

---
+++ todoapp/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=FileResponse)
+def redirect_index():
+    return "static/index.html"
+
